b483b3408e4813298e5c4cad2426fb5319d82b1f
Yutt43 <yutt_43@hotmail.com>
2015-12-09 11:47:18 +0700

    Login เสดและ

--- app/src/main/java/swu/sun/yut/ob/drivingbetter/MainActivity.java
+++ app/src/main/java/swu/sun/yut/ob/drivingbetter/MainActivity.java
@@ -1,5 +1,7 @@
 package swu.sun.yut.ob.drivingbetter;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        },2000);
+
     }
 }
