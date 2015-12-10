e8f89f781a5c2d6f711b86954e409b9f6be97671
Yutt43 <yutt_43@hotmail.com>
2015-12-10 09:31:44 +0700

    Start 10/12/15

--- app/src/main/java/swu/sun/yut/ob/drivingbetter/LoginActivity.java
+++ app/src/main/java/swu/sun/yut/ob/drivingbetter/LoginActivity.java
@@ -1,7 +1,9 @@
 package swu.sun.yut.ob.drivingbetter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void clickSignUp(View view) {
+        Intent objIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(objIntent);
+    }
+
+
+
 }
